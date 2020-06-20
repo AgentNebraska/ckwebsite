380ce6260dc043c210d52bf74f661865f5be843d
Connor Raikar <AgentRaikar@gmail.com>
2020-06-20 17:48:04 -0500

    Added ourteam.html and ourevents.html to flask framework and adjusted

--- app.py
+++ app.py
@@ -7,9 +7,20 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     displayBackground="Images/bread.jpg"
-    return render_template("index.html", displayBackground=displayBackground)
+    backgroundID="homeDisplayBackground"
+    centeredText="Fighting waste, feeding people."
+    return render_template("index.html", displayBackground=displayBackground, backgroundID=backgroundID, centeredText=centeredText)
 
 @app.route("/ourteam")
 def ourteam():
     displayBackground="Images/LTmembers.JPG"
-    return render_template("ourteam.html", displayBackground=displayBackground)
+    backgroundID="ourteamDisplayBackground"
+    centeredText="Our Team"
+    return render_template("ourteam.html", displayBackground=displayBackground, backgroundID=backgroundID, centeredText=centeredText)
+
+@app.route("/ourevents")
+def ourevents():
+    displayBackground="Images/pizza.jpg"
+    backgroundID="homeDisplayBackground"
+    centeredText="Our Events"
+    return render_template("ourevents.html", displayBackground=displayBackground, backgroundID=backgroundID, centeredText=centeredText)
